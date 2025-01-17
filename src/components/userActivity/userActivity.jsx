f62af039c2466702618973d266f21892fc7700c2
Mihaela <talmazanmihaela@gmail.com>
2025-01-17 15:56:30 +0100

    feat: display dynamic post count in UserActivity component

--- src/components/userActivity/userActivity.jsx
+++ src/components/userActivity/userActivity.jsx
@@ -1,10 +1,12 @@
 import styles from "./userActivity.module.css";
+import { useSelector } from "react-redux";
 
 function UserActivity() {
+  const postImages = useSelector((state) => state.postImages.images);
   return (
     <div className={styles.profile_content}>
       <div className={styles.stat}>
-        <p className={styles.stat_number}>0</p>
+        <p className={styles.stat_number}>{postImages.length}</p>
         <p className={styles.stat_label}>posts</p>
       </div>
       <div className={styles.stat}>
